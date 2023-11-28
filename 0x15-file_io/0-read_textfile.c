25db82ee0a4dd4a151557a2779329172e56bd235
Atuti-Brian <atutibrian729@gmail.com>
2023-11-28 03:32:14 -0800

    C-file O/I update 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,7 @@
-#include <stddef.h>
-#include <unistd.h>
-#include <stdlib.h>  /* Add this line for malloc and free */
+#include "main.h"
+#include <stddef.h>  /* Add this line for size_t */
+#include <unistd.h>  /* Add this line for ssize_t */
 #include <fcntl.h>   /* Add this line for open */
-#include <stdio.h>   /* Add this line for perror */
 
 /**
  * read_textfile - reads a text file and prints the letters
@@ -13,46 +12,27 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nrd, nwr;
-    char *buf;
+int fd;
+ssize_t nrd, nwr;
+char *buf;
 
-    if (!filename)
-        return (0);
+if (!filename)
+return (0);
 
-    fd = open(filename, O_RDONLY);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
 
-    if (fd == -1) {
-        perror("Error opening the file");
-        return (0);
-    }
+buf = malloc(sizeof(char) * letters);
+if (!buf)
+return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    if (!buf) {
-        perror("Error allocating memory");
-        close(fd);
-        return (0);
-    }
+nrd = read(fd, buf, letters);
+nwr = write(STDOUT_FILENO, buf, nrd);
 
-    nrd = read(fd, buf, letters);
-    if (nrd == -1) {
-        perror("Error reading from file");
-        close(fd);
-        free(buf);
-        return (0);
-    }
+close(fd);
 
-    nwr = write(STDOUT_FILENO, buf, nrd);
-    if (nwr == -1) {
-        perror("Error writing to stdout");
-        close(fd);
-        free(buf);
-        return (0);
-    }
+free(buf);
 
-    close(fd);
-    free(buf);
-
-    return (nwr);
+return (nwr);
 }
-
