b6e4441d034b9d3da1a6278d631814bdce7b286c
Atuti-Brian <atutibrian729@gmail.com>
2024-01-15 23:54:45 -0800

    c-files_containing_functions

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry
+ * Return: 0
+ * @c: loop
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x18-dynamic_libraries/100-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *_strcmp- Entry
+  * @s1: ch1
+  * @s2: ch2
+  * Return: 0
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 == 'a')
+		return (1);
+	if (*s2 == 'b')
+		return (2);
+	return (1);
+}
